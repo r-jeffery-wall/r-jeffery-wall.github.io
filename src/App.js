29e0a7cf8d94d5fdb7403aa061a3346ea9bcc9da
r-jeffery-wall <rowjw97@gmail.com>
2024-02-15 19:04:20 +0000

    Initial file commit.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <Background />
+    <Banner />
+    <Nav />
+    <About />
+    <Projects />
+    <Contributions />
+    <Socials />
+  </>
+  );
+}
+
+export default App;
