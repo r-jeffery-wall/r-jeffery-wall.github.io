feb9f78852e15950fef863279384ff56820b244c
r-jeffery-wall <rowjw97@gmail.com>
2024-02-17 12:40:57 +0000

    Fleshed out project display logic.

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ function Project(props) {
         <div class="project-container">
             <img src={props.image.path} alt={props.image.alt} class="img-left" />
                 <div class="content-container">
-                    <h3>{props.name}</h3>
+                    <h3>{props.url ? <a href={props.url} target="_blank">{props.name}</a> : props.name }</h3>
                     <p>{props.text}</p>
                 </div>
         </div>
