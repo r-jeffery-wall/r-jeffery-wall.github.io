5c318cf6c560c055173b34a6b5e504b21ed9738d
r-jeffery-wall <rowjw97@gmail.com>
2024-02-26 20:07:21 +0000

    Updated site content.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
                 <h2 class="section-heading">About</h2>
                 <p>I have always had an interest in coding, but in the last year or so I have been dedicated to learning a variety of full-stack technologies and building projects in both a professional and hobbyist capacity. I have enjoyed learning a variety of technologies and using projects as a learning tool and a way to discover new technologies. Below you can find a few examples of key projects I have worked on, and a full list of my work can be found on my <a href="https://github.com/r-jeffery-wall" target="_blank">GitHub profile</a>.</p>
                 <p>Alongside coding and web development, my hobbies include gaming, photography, and board games.</p>
-                <p>This site is actively under construction and will change and evolve as my skills grow and I learn new technologies.</p>
+                <p>This site is actively under construction and will change and evolve as my skills grow and I learn new technologies. The current iteration of this site uses React 18.</p>
             </div>
         </section>
     )
