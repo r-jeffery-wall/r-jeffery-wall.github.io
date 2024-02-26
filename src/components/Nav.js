3e4177d9175c5c36395a0bdab163705d8e55a58f
r-jeffery-wall <rowjw97@gmail.com>
2024-02-26 21:55:05 +0000

    Updated icons. Fixed Navbar.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 function Nav() {
     return (
         <nav class="content-container">
-            <h2 id="secondary-header">Rowan Jeffery-Wall</h2>
+            {/* <h2 id="secondary-header">Rowan Jeffery-Wall</h2> */}
             <ul class="nav-links">
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#about">About</a></li>
