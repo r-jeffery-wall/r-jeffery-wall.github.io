78cbb7d49e7f6cd60ae1bd36d47ad36114b708b3
r-jeffery-wall <rowjw97@gmail.com>
2024-05-08 21:32:08 +0100

    Updated hero font and nav active colours.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,10 +2,10 @@ import { SkillsList } from "../skillsList/SkillsList"
 
 export const Hero = () => {
     return (
-       <div id="site-hero" className="p-0 m-0 text-center young-serif-regular d-flex flex-column justify-content-center align-items-center text-white fw-bold">
-            <h1>A motivated and well-rounded junior full-stack developer.</h1>
-            <h3>Skilled in React front-end development and various back-end technologies.</h3>
-            <h3>My skills include:</h3>
+       <div id="site-hero" className="p-0 m-0 text-center montserrat-regular d-flex flex-column justify-content-center align-items-center text-white fw-bold">
+            <h1 className="text-uppercase">A motivated and well-rounded junior full-stack developer.</h1>
+            <h3 className="text-uppercase">Skilled in React front-end development and various back-end technologies.</h3>
+            <h3 className="text-uppercase">My skills include:</h3>
             <SkillsList />
        </div> 
     )
