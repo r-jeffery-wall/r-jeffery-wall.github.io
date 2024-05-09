d0c9fc89e8d8ee42831d9030a7637c00ffc93b1f
r-jeffery-wall <rowjw97@gmail.com>
2024-05-09 22:08:59 +0100

    Updated skills list styling.

--- src/components/skillsList/Skill.tsx
+++ src/components/skillsList/Skill.tsx
@@ -1,10 +1,13 @@
 export const Skill = ({ skill }: { skill: string }) => {
-    const image = require(`../../assets/${skill}.png`);
-    
-    return (
-        <li key={skill} className="m-3 rounded-circle p-3 d-flex flex-column justify-content-center align-items-center col translucent img-hover" style={{maxWidth: "115px", maxHeight: "115px", backgroundColor: "rgba(255, 255, 255, 0.68)"}}>
-           <img src={image} alt={`${skill} logo.`} className="mb-2 w-50 h-50 ratio ratio-1x1" />
-           <p className="text-dark" >{skill}</p>
-        </li>
-    )
+  const image = require(`../../assets/${skill}.png`)
+
+  return (
+    <li
+      key={skill}
+      className="m-3 p-3 col img-hover rounded"
+    >
+      <img src={image} alt={`${skill} logo.`} className="mb-2" style={{width: "50px", height: "50px"}}/>
+      <p>{skill}</p>
+    </li>
+  )
 }
--- src/components/skillsList/SkillsList.tsx
+++ src/components/skillsList/SkillsList.tsx
@@ -1,13 +1,18 @@
-import { useSelector } from "react-redux"
-import { selectSkills } from "../../features/skills/SkillsSlice"
-import { Skill } from "./Skill";
+import { useSelector } from 'react-redux'
+import { selectSkills } from '../../features/skills/SkillsSlice'
+import { Skill } from './Skill'
 
 export const SkillsList = () => {
-    const skills = useSelector(selectSkills);
+  const skills = useSelector(selectSkills)
 
-    return (
-      <ul className="d-flex justify-content-between align-items-center list-unstyled flex-wrap">
-        {skills.map(skill => <Skill skill={skill} />)}
-      </ul>          
-    )
+  return (
+    <ul
+      className="d-flex justify-content-between align-items-center list-unstyled flex-wrap"
+      id='skills-list'
+    >
+      {skills.map((skill) => (
+        <Skill skill={skill} />
+      ))}
+    </ul>
+  )
 }
