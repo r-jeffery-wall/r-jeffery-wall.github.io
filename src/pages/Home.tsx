e01bc07d46eb244b54033ac0c5bb298f7c54d73c
r-jeffery-wall <rowjw97@gmail.com>
2024-05-10 19:12:16 +0100

    Added about biography section.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { About } from '../components/about/About'
 import { Hero } from '../components/hero/Hero'
 
 export const Home: React.FC = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
