4c9aebc519e68df4b5192bd008fad8b9523f7d30
r-jeffery-wall <rowjw97@gmail.com>
2024-05-10 19:21:23 +0100

    Updated footer text.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer: React.FC = () => {
           <Social name={social.name} url={social.url} icon={social.icon} />
         ))}
       </ul>
-      <p className='text-secondary'>Designed and developed by Rowan Jeffery-Wall. Built with <a className='link-secondary' href='https://react.dev' target='_blank'>React</a>, <a className='link-secondary' href='https://redux.js.org' target='_blank'>Redux</a>, and <a className='link-secondary' href='https://reactrouter.com/en/main' target='_blank'>React-Router</a>. Icons from <a className='link-secondary' href='https://fontawesome.com' target='_blank'>Font Awesome</a>. Site repo at <a className='link-secondary' href='https://github.com/r-jeffery-wall/r-jeffery-wall.github.io' target='_blank'>GitHub</a>.</p>
+      <p className='text-secondary'>Designed and developed by Rowan Jeffery-Wall. Built with <a className='link-secondary' href='https://react.dev' target='_blank'>React</a>, <a className='link-secondary' href='https://redux.js.org' target='_blank'>Redux</a>, and <a className='link-secondary' href='https://reactrouter.com/en/main' target='_blank'>React-Router</a>. Icons from <a className='link-secondary' href='https://fontawesome.com' target='_blank'>Font Awesome</a>. Site repo at <a className='link-secondary' href='https://github.com/r-jeffery-wall/r-jeffery-wall.github.io' target='_blank'>GitHub</a>. All photos used are my own.</p>
     </Container>
   )
 }
