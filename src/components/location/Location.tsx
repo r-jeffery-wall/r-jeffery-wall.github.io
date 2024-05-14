ca7d222a393d847e6ba2a760996320cc651a7419
r-jeffery-wall <rowjw97@gmail.com>
2024-05-14 11:59:30 +0100

    Changed emoji position on location section.

--- src/components/location/Location.tsx
+++ src/components/location/Location.tsx
@@ -4,7 +4,7 @@ export const Location: React.FC = () => {
       id="location"
       className="p-0 m-0 text-center about-section d-flex flex-column justify-content-center align-items-center text-white text-center fw-bold border-top border-bottom border-black border-3"
     >
-        <h3 className="mb-5">Currently based in: 🌍</h3>
+        <h3 className="mb-5">🌍 Currently based in: </h3>
         <h1>London, United Kingdom</h1>
     </div>
   )
