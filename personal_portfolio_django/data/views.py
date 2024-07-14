46f2341a0ec8ec3c85ae6b840deef1bf0a0f01fa
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-14 14:56:00 +0100

    Added basic API request for Skills.

--- personal_portfolio_django/data/views.py
+++ personal_portfolio_django/data/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Skill
+from .serializers import *
 
 # Create your views here.
+
+@api_view(['GET'])
+def get_skills(request):
+    if request.method == 'GET':
+        data = Skill.objects.all()
+
+        serializer = SkillSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
