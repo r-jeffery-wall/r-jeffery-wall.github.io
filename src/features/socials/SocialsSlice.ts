f0f53ac82f6f83935cc5e633bac93fe1b34669bc
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-20 11:13:50 +0100

    Implemented API for socials.

--- src/features/socials/SocialsSlice.ts
+++ src/features/socials/SocialsSlice.ts
@@ -1,5 +1,11 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { RootState } from '../../store'
+
+interface SocialsState {
+  socialsLoading: boolean
+  socialsLoadFailed: boolean
+  socialsList: SocialInterface[]
+}
 
 export interface SocialInterface {
   name: string
@@ -7,33 +13,45 @@ export interface SocialInterface {
   icon: string
 }
 
+export const getSocialsList = createAsyncThunk(
+  'socials/getSocialsList',
+  async () => {
+    const response = await fetch('http://localhost:8000/api/socials/')
+    const json = await response.json()
+    return json
+  }
+)
+
+const initialState: SocialsState = {
+  socialsLoading: false,
+  socialsLoadFailed: false,
+  socialsList: [],
+}
+
 const socialsSlice = createSlice({
   name: 'socials',
-  // Dummy values
-  initialState: [
-    {
-      name: 'Email',
-      url: 'mailto: rowjw97@gmail.com',
-      icon: 'fa-solid fa-envelope',
-    },
-    {
-      name: 'GitHub',
-      url: 'https://github.com/r-jeffery-wall',
-      icon: 'fa-brands fa-github',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/rowphotojw/',
-      icon: 'fa-brands fa-instagram',
-    },
-    {
-      name: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/rowan-jeffery-wall-822260257/',
-      icon: 'fa-brands fa-linkedin',
-    },
-  ],
+  initialState: initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getSocialsList.pending, (state) => {
+      state.socialsLoading = true
+      state.socialsLoadFailed = false
+    })
+    builder.addCase(getSocialsList.fulfilled, (state, action) => {
+      state.socialsLoading = false
+      state.socialsLoadFailed = false
+      state.socialsList = action.payload
+    })
+    builder.addCase(getSocialsList.rejected, (state) => {
+      state.socialsLoading = false
+      state.socialsLoadFailed = true
+    })
+  },
 })
 
-export const selectSocials = (state: RootState) => state.socials
+export const selectSocials = (state: RootState) => state.socials.socialsList
+export const selectSocialsLoading = (state: RootState) =>
+  state.socials.socialsLoading
+export const selectSocialsLoadFailed = (state: RootState) =>
+  state.socials.socialsLoadFailed
 export default socialsSlice.reducer
