b82407b8da3037e48403e91acaa01c3963129af3
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-07-20 11:21:51 +0100

    Changed copy to reflect the use of Django.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 This is the source code for my portfolio page where I showcase my skills and projects.
 
 ## Project Details
-The current iteration of this project is implemented using React/Redux and Typescript. Currently I have supplied primarily static data to Redux (barring the GitHub API integration). It is my intention to implement a backend that will serve dynamic data.
+The current iteration of this project is implemented using React/Redux and Typescript. I have also built a Django backend to serve skills, projects, and socials data. I use the built-in Django admin page to interact with and update this data.
 
 ## Next Steps
-- Implement a Django back-end to store and modify dynamic data for the page (project lists, skills, etc.)
+- Refactor Django backend to use best practices (uploading of images from admin page, etc.)
+- Add additional features to the site.
--- src/components/siteDescription/SiteDescription.tsx
+++ src/components/siteDescription/SiteDescription.tsx
@@ -4,9 +4,7 @@ export const SiteDescription: React.FC = () => {
       id="site-description"
       className="p-3 m-0 text-center about-section d-flex flex-column justify-content-center align-items-center text-white text-center fw-bold border-top border-bottom border-black border-3"
     >
-      <div
-        className="container-lg border border-secondary rounded p-2 about-copy-container"
-      >
+      <div className="container-lg border border-secondary rounded p-2 about-copy-container">
         <h2 className="mb-5">About this Site</h2>
         <p>
           This site is intended to be a showcase of my skills, projects I have
@@ -16,18 +14,9 @@ export const SiteDescription: React.FC = () => {
         </p>
         <p>
           The current iteration of this site is built using React 18, Redux, and
-          React-Router. Currently, the data fed into Redux is mostly static, but
-          as my back-end skills grow I intend to build out a back-end that will
-          allow me to update the list of skills and projects on this site using
-          an admin dashboard and API calls. I am currently learning{' '}
-          <a href="https://expressjs.com/" target="_blank">
-            Express
-          </a>
-          , but I may also explore other back-end technologies such as{' '}
-          <a href="https://www.djangoproject.com/" target="_blank">
-            Django
-          </a>
-          .
+          React-Router. The site has a Django backend that serves data for the
+          skills, projects, and socials that are displayed on the site. I also
+          pull data from the GitHub API. .
         </p>
         <p>
           This site is the first project that I have used TypeScript for. I have
