696110d05affbe115d37faafcddcf0c6139414bc
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-09-13 19:59:44 +0100

    Removed database from backend db container.

--- api/personal_portfolio_django/personal_portfolio_django/settings.py
+++ api/personal_portfolio_django/personal_portfolio_django/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'personal_portfolio_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
