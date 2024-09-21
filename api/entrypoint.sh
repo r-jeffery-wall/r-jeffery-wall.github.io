a10854040a1e8a05b99feb9c2d9399767ea87648
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-09-21 18:06:59 +0100

    Changed structure of backend.

--- api/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python ./personal_portfolio_django/manage.py makemigrations
-python ./personal_portfolio_django/manage.py migrate --no-input
-python ./personal_portfolio_django/manage.py collectstatic --no-input
-
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
-
