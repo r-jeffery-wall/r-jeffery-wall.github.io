1ba0694722dd030c757ad7ae6639c39f9817fb18
Rowan Jeffery-Wall <rowjw97@gmail.com>
2024-09-24 22:48:56 +0100

    Corrected API URL for frontend API calls.

--- frontend/src/features/host.ts
+++ frontend/src/features/host.ts
@@ -1,3 +1,3 @@
 import json from './settings.json'
 
-export const host = json.prod ? 'https://www.rowanjefferywall.com' : 'http://localhost:8000'
+export const host = json.prod ? 'https://rowanjefferywall.com' : 'http://localhost:8000'
