54427b64a65ce8c3ba1351c025ff3fa2572f5487
Rowan <rowjw97@gmail.com>
2026-09-07 22:08:23 +0100

    Updated About section.

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -2,37 +2,23 @@ import Section from './Section'
 
 export default function About() {
   return (
-    <Section title="~/about.sh" subtitle="Hi, I'm Rowan.">
+    <Section title="~/about.sh" subtitle="A technology and IT generalist">
       <p>
         Although I have always been interested in technology and coding, my
         coding journey began in earnest two years ago. I began with Python so
-        that I could work on Raspberry Pi projects. I have since started with
-        full-stack web development, learning JavaScript and moving on to
-        front-end frameworks such as React and back-end technologies such as
-        Express.
+        that I could work on Raspberry Pi projects. I now work in workflow automation and AI engineering, using traditional coding methods and scripting to build automations as well as learning as much as I can about AI and agentic workflows and development.
       </p>
       <p>
-        I am motivated by a desire to learn new things and to learn by building
-        things. There's no better way to learn than getting stuck in and
-        working it out! I am also deeply interested in the free and open-source
-        eco-system and would love to contribute to some projects as my skills
-        grow. I am also trying to learn best practices as I go so that my code
-        can be accessible and potentially useful for as many as possible.
+        As part of this work I have come to appreciate the importance of creating robust infrastructure for the deploymnent of AI workflows. I have begun to invest more time into learning in this direction as I believe that without robust infrastructure practices for agentic AI to run in we will not be able to achieve the maximum productivity gains that can be achieved with agentic AI.
       </p>
       <p>
-        I am also interested in photography, cinema, gaming, urbanism, and
-        history. Please feel free to explore my projects and contributions
-        below, and consult the social links at the bottom of the page to see
-        more and get in touch!
+        Alongisde all this I have a deep interest in self-hosting my own projects. This has helped me to learn the core concepts of hosting reliable and maintainable infrastructure, and I am working towards self-hosting my own AI agents. I am also a great believer in open source software projects and I am interested to see what innovations will come out of the open source space in the agentic AI sphere.
       </p>
       <p>
-        The current iteration of this site is built using React, Redux, and
-        Tailwind CSS, with Vite as the build tool. All content is served from a
-        static config file, and contribution data is pulled from the GitHub
-        API.
+        I would describe myself as a technology generalist. I am also interested in self-built microcomputing projects using things like Raspberry Pi and ESP32. I also enjoy working on game development in the Godot engine. Outside of the world of technology I am interested in classic cinema, and I currently work on A/V for screenings.
       </p>
-      <p className="text-2xl font-bold">
-        🌍 Currently based in: London, United Kingdom
+      <p className="font-doto text-orange-400 text-2xl font-bold">
+        ~/location.sh: London, United Kingdom
       </p>
     </Section>
   )
